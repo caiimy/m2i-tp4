00f177cace76fe6bc34d566953956cfaa360805c
houssamD <houssam.darrab@labom2iformation.fr>
2023-10-24 18:50:30 +0200

    Ajout des fichier terraform pour les VM

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,8 @@
+# Account region
+region = "europe_west1"
+
+#Account zone
+zone = "c"
+
+# Project name
+project_name = "m2i-tp4"
---
+++ terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "gcp_user"{
+    type = string
+    default = "admin"
+}
+
+variable "region"{
+    type = string
+}
+
+variable "zone"{
+    type = string
+}
+
+variable "project_name"{
+    type = string
+}
