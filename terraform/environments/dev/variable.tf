a59dea7986bed908fee54030be96c3fc62e1cc13
Houss <houssam.darrab@labom2iformation.fr>
2023-11-17 17:39:25 +0100

    guillemet oublié git add -A!

---
+++ terraform/environments/dev/variable.tf
@@ -0,0 +1,21 @@
+# Account Environment
+variable "env" {
+    type = string
+}
+
+variable "gcp_user" {
+    type = string
+    default = "admin"
+}
+
+variable "region" {
+    type = string
+}
+
+variable "zone" {
+    type = string
+}
+
+variable "project_name" {
+    type = string
+}
