d7bf4144416a16a872f13b4cdf035e51a86b2af6
Houss <houssam.darrab@labom2iformation.fr>
2023-11-17 17:20:35 +0100

    Variable terraform dev deploy test hello word

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,26 +1,26 @@
 # Account Environment
-variable "env"{
+variable "env" {
     type = string
 }
 
 # Kubernetes cluster prod name
-variable "cluster_name{
+variable "cluster_name {
     type = string
 }
 
-variable "gcp_user"{
+variable "gcp_user" {
     type = string
     default = "admin"
 }
 
-variable "region"{
+variable "region" {
     type = string
 }
 
-variable "zone"{
+variable "zone" {
     type = string
 }
 
-variable "project_name"{
+variable "project_name" {
     type = string
 }
