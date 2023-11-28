527dced4a621fc6662d558bf42b44b010766466f
Houss <houssam.darrab@labom2iformation.fr>
2023-11-28 19:01:38 +0100

    test import terraform ssh

---
+++ terraform/test/variable.tf
@@ -0,0 +1,23 @@
+# Account Environment
+variable "env" {
+    type = string
+}
+
+variable "gcp_user" {
+    type = string
+    default = "admin"
+}
+
+variable "region" {
+    type = string
+}
+
+variable "zone" {
+    type = string
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "credentials_file" { }
