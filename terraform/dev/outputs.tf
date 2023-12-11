2e1876bdc821ad9511f9471ce91ee5054cbe80d2
Houss <houssam.darrab@labom2iformation.fr>
2023-12-11 15:04:06 +0100

    output external ip

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -2,6 +2,10 @@ output "vm_intern_ip" {
   value = google_compute_instance.vm.network_interface[0].network_ip
 }
 
+output "vm_extern_ip" {
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
 output "vm_ip" {
   value = google_compute_address.vm_ip.address
 }
