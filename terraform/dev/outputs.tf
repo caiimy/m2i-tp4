3a9e53d80f8245db0b2d2a95a25460107d2383f3
Houss <houssam.darrab@labom2iformation.fr>
2023-12-12 16:53:44 +0100

    get user ssh

--- terraform/dev/outputs.tf
+++ terraform/dev/outputs.tf
@@ -5,3 +5,7 @@ output "vm_intern_ip" {
 output "vm_extern_ip" {
   value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
 }
+
+output "vm_username" {
+  value = google_compute_instance.vm.metadata.[0]
+}
