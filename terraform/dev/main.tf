9113812732055fef1df18d2a2b29155683c0d339
Houss <houssam.darrab@labom2iformation.fr>
2023-12-15 15:30:21 +0100

    ansible problem fix

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -60,7 +60,7 @@ resource "google_compute_instance" "vm" {
   }
 
   metadata = {
-    ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${file("~/.ssh/id_rsa")}"
+    ssh-keys = "${split("@", data.google_client_openid_userinfo.me.email)[0]}:${file("~/.ssh/id_rsa.pub")}"
   }
 
   service_account {    
